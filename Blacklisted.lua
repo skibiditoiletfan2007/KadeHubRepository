c4a90acae20bf58418712b2b2ea13a3e7dc4217b
skibiditoiletfan2007 <148602068+skibiditoiletfan2007@users.noreply.github.com>
2024-07-11 03:57:14 -0700

    Blacklisted Members

---
+++ Blacklisted.lua
@@ -0,0 +1,7 @@
+local BlacklistedClients = {
+    "C078E5FE-701F-4FAA-ACA3-B4E8FF634CB9",
+    "0D2FE39E-B62A-4087-BE54-49AB053AD808",
+    "B68A1E27-CDCE-486C-B84C-427481248A96",
+}
+
+return BlacklistedClients
