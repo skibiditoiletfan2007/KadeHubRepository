c82dcabf786153276746191454e50dfee8605596
skibiditoiletfan2007 <148602068+skibiditoiletfan2007@users.noreply.github.com>
2024-07-11 04:35:43 -0700

    "rape trauma"

--- Blacklisted.lua
+++ Blacklisted.lua
@@ -1,6 +1,7 @@
 local BlacklistedClients = {
     "C078E5FE-701F-4FAA-ACA3-B4E8FF634CB9",
     "0D2FE39E-B62A-4087-BE54-49AB053AD808",
+    "8217E0C9-7580-47F3-814A-A737DC5035EA",
 }
 
 return BlacklistedClients
