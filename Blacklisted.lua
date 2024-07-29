12b9f6c793b7ffb668deadea45de2e59de9ecd4d
skibiditoiletfan2007 <148602068+skibiditoiletfan2007@users.noreply.github.com>
2024-07-29 01:56:03 -0700

    another

--- Blacklisted.lua
+++ Blacklisted.lua
@@ -5,6 +5,7 @@ local BlacklistedClients = {
     "0EC7BEAA-EB5C-4117-A193-D32783D1502B",
     "149D0678-AD68-4F41-B518-0C203B7F22E7",
     "fa194e59-c934-49fe-9b46-a1adefe3b120",
+    "bcf1e64d-f463-4a9a-b962-2ef8fb3409fa",
 }
 
 return BlacklistedClients
